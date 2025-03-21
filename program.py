f12ec74c66dcf9aacb864d6dbd7e3c0673e5fa1e
Sofia <sofiaborodkina@gmail.com>
2025-03-21 23:34:04 +0100

    added show_tasks func and delete_task func

--- program.py
+++ program.py
@@ -24,19 +24,35 @@ def add_task(task: str,priority: str,status: str) -> bool:
     """
     if priority in PRIORITY and status in STATUS:
         tasks[len(tasks)+1] = {"task":task,"priority":priority,"status":status}
+        return True
+    else:
+        return False
 
 def delete_task(id: int) -> bool:
     """
     :param id: id of the task
     :return: true if successful
     """
+    if id in tasks:
+        del tasks[id]
+        return True
+    else:
+        return False
+def show_tasks():
+    if not tasks:
+        print("No tasks available.")
+        return
 
+    for id, details in tasks.items():
+        print(f"id: {id} \ntask: {details["task"]} \npriority: {PRIORITY[details["priority"]]} \nstatus: {STATUS[details['status']]}")
+        print("-"*25)
 
-def update_task(id,num):
+def update_task(id, **kwargs):
+    pass
+def update_task(id,**kwargs):
     pass
 
 def save():
     pass
-
-add_task("hw","1","1")
-print(tasks[1])
+add_task("neco","1","1")
+show_tasks()
